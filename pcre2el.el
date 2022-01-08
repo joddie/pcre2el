ffa53a7c78c55bfb068cdfb68ca8b1705c119f82
Stefan Kangas <stefan@marxist.se>
2022-01-08 10:57:00 +0100

    Bump version to 1.9

--- pcre2el.el
+++ pcre2el.el
@@ -6,7 +6,7 @@
 ;; Hacked additionally by:	opensource at hardakers dot net
 ;; Created:			14 Feb 2012
 ;; Updated:			13 December 2015
-;; Version:                     1.8
+;; Version:                     1.9
 ;; Url:                         https://github.com/joddie/pcre2el
 ;; Package-Requires:            ((emacs "24") (cl-lib "0.3"))
 
